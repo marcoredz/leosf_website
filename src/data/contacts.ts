dbafea921c08e4fc12235cde110d86fe7f1f1407
Marco Rossi <marcoreds9724@gmail.com>
2026-08-01 12:51:05 +0200

    Refactor contact details and social links; add participation page and form component

---
+++ src/data/contacts.ts
@@ -0,0 +1,32 @@
+export const contactDetails = [
+	{
+		label: "Email",
+		value: "info@endofsummerfestival.it",
+		href: "mailto:info@endofsummerfestival.it",
+		external: false,
+	},
+	{
+		label: "Telefono",
+		value: "+39 379 318 7294",
+		href: "tel:+393793187294",
+		external: false,
+	},
+] as const;
+
+export const socials = [
+	{
+		label: "Instagram",
+		href: "https://www.instagram.com/lavenoendofsummerfestival",
+		icon: "simple-icons:instagram",
+	},
+	{
+		label: "TikTok",
+		href: "https://vm.tiktok.com/ZNRTJPnRQ/",
+		icon: "simple-icons:tiktok",
+	},
+	{
+		label: "Spotify",
+		href: "https://open.spotify.com/playlist/6eCBnz6tWHKffBUsiDtQts/",
+		icon: "simple-icons:spotify",
+	},
+] as const;
